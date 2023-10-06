6d78317fac4d58ec16c7b3b40ad1753382eec42a
BEUKEMANW22 <beukemanw22@gcc.edu>
2023-10-06 13:36:29 -0400

    Updated to greet by name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Enter your name, Aiden:");
+
+        Scanner scnr = new Scanner(System.in);
+        String name = scnr.next();
+
+        System.out.println("Hello " + name);
+
     }
 }
